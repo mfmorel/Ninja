766b4497433c2301a0ed25b38b596ce8ae4fa771
Michat <Michat@MICHAT-PC>
2017-10-12 17:43:32 +0200

    Added grid view for ninja Added delete ninja

---
+++ Ninja/Model/INinja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninja.ViewModel;
+
+namespace Ninja.Model
+{
+    interface INinja
+    {
+        List<Domain.Ninja> GetNinjas();
+        void DeleteNinja(String name);
+        Domain.Ninja GetNinja(String name);
+    }
+}
---
+++ Ninja/Model/NinjaRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Ninja.ViewModel;
+
+namespace Ninja.Model
+{
+    class NinjaRepository : INinja
+    {
+        public void DeleteNinja(string name)
+        {
+            Domain.Ninja ninjatodelete = GetNinja(name);
+
+            using (var context2 = new NinjaEntities())
+            {
+                context2.Entry(ninjatodelete).State = System.Data.Entity.EntityState.Deleted;
+                context2.SaveChanges();
+            }
+        }
+
+        public Domain.Ninja GetNinja(string name)
+        {
+            using (var context = new NinjaEntities())
+            {
+                return context.Ninjas.FirstOrDefault(s => s.Name == name);
+            }
+        }
+
+        public List<Domain.Ninja> GetNinjas()
+        {
+            using (var context = new NinjaEntities())
+            {
+                return context.Ninjas.ToList();
+            }
+        }
+
+    }
+}
